3c3f9e5ec0b1bc83f922d6a8ac2a78f8d62f1379
Ivan Biryuk <vanyabiryuk@icloud.com>
2022-07-30 00:48:31 +0500

    Initial commit

---
+++ Faces/FacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacesApp.swift
+//  Faces
+//
+//  Created by Vanüsha on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
